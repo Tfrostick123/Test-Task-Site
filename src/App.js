35847d33ff5884e70a2e6388719c3769a2681764
Tom Frostick <tomfrostick@hotmail.co.uk>
2021-09-16 21:57:29 +0100

    Mac commit 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm/LoginForm';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name: "", email:""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email === adminUser.email
+        && details.password === adminUser.password) {
+          console.log("Logged in")
+          setUser({
+            name: details.name,
+            email: details.email
+          })
+          setError("");
+        } else {
+          console.log("Details do not match");
+          setError("Details do not match. Please try again.");
+        }
+  }
+
+  const Logout = () => {
+    console.log("Logout");
+    setUser({ name: "", email: ""});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email !== "") ? (
+        <Router>
+          <Navbar user={user} Logout={Logout}/>
+        </Router>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
-
+/** poops */
 export default App;
